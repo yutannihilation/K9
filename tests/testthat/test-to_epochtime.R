035b6f7f66a7f95705ff02e70c292458399b6621
Hiroaki Yutani <yutani.ini@gmail.com>
2017-06-05 01:10:50 +0900

    fix a typo

--- tests/testthat/test-to_epochtime.R
+++ tests/testthat/test-to_epochtime.R
@@ -1,23 +1,23 @@
-context("to_epocktime")
+context("to_epochtime")
 
-test_that("to_epocktime with numeric works", {
+test_that("to_epochtime with numeric works", {
   expect_equal(to_epochtime(1496587909), 1496587909L)
   expect_equal(to_epochtime(1496587909L), 1496587909L)
 })
 
-test_that("to_epocktime with POSIXct and POSIXlt works", {
+test_that("to_epochtime with POSIXct and POSIXlt works", {
   x <- structure(1496587909, class = c("POSIXct", "POSIXt"), tzone = "UTC")
   expect_equal(to_epochtime(x), 1496587909L)
   x <- as.POSIXct(x)
   expect_equal(to_epochtime(x), 1496587909L)
 })
 
-test_that("to_epocktime with Date works", {
+test_that("to_epochtime with Date works", {
   x <- as.Date("2017-06-04", "%Y-%m-%d", tz = "UTC")
   expect_equal(to_epochtime(x), 1496534400L)
 })
 
-test_that("to_epocktime with character works", {
+test_that("to_epochtime with character works", {
   x <- "2017-06-04"
   y <- as.integer(as.POSIXct(strptime("2017-06-04", "%Y-%m-%d")))
   expect_equal(to_epochtime(x), y)
@@ -27,10 +27,10 @@ test_that("to_epocktime with character works", {
   expect_equal(to_epochtime(x), y)
 })
 
-test_that("to_epocktime with NULL throws error", {
+test_that("to_epochtime with NULL throws error", {
   expect_error(to_epochtime(NULL))
 })
 
-test_that("to_epocktime with elements more than 1 length throws error", {
+test_that("to_epochtime with elements more than 1 length throws error", {
   expect_error(to_epochtime(1:2))
 })
